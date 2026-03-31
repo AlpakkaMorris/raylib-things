556d2248e9b20351ea836bdbbc68432a0cbb8081
AlpakkaMorris <danielkharitonov@mail.ru>
2026-04-01 00:01:38 +0500

    t'

--- analog-clock/src/settings-window.c
+++ analog-clock/src/settings-window.c
@@ -4,24 +4,33 @@
 #include "../include/raygui.h"
 #include "../include/settings-window.h"
 
-Rectangle settingsWindowMainRect = { 50, 50, 300, 300 };
-Rectangle settingsWindowContentRect = {26, 26, 440, 440 };
-Rectangle settingsWindowView = { 0 };
-Vector2 settingsWindowScroll = { 99, -20 };
+Rectangle settingsWindowGuiWindowBoxRect = { 48, 48, 336, 220 };
+Rectangle settingsWindowGuiScrollPanelRect= {48, 72, 336, 220 };
+Rectangle settingsWindowGuiScrollPanelView = { 0 };
+Vector2 settingsWindowGuiScrollPanelScroll = { 99, 20 };
 int settingsWindowElementBaseOffset = 50;
 
 void DrawSettingsWindow(bool *isSettingsWindowOpened)
   {
   if (*isSettingsWindowOpened)
   {
-    int result = GuiWindowBox(settingsWindowContentRect, "title");
+    int result = GuiWindowBox(settingsWindowGuiWindowBoxRect, "title");
 
-    GuiScrollPanel(settingsWindowMainRect, NULL, settingsWindowContentRect, &settingsWindowScroll, &settingsWindowView);
+    GuiScrollPanel(settingsWindowGuiScrollPanelRect, NULL, settingsWindowGuiWindowBoxRect, &settingsWindowGuiScrollPanelScroll, &settingsWindowGuiScrollPanelView);
 
-    BeginScissorMode(settingsWindowView.x, settingsWindowView.y, settingsWindowView.width, settingsWindowView.height);
-      //GuiGrid((Rectangle){settingsWindowMainRect.x + settingsWindowScroll.x, settingsWindowMainRect.y + settingsWindowScroll.y,   settingsWindowContentRect.width, settingsWindowContentRect.height}, NULL, 16, 3, NULL);
-      //int res = GuiButton((Rectangle){settingsWindowElementBaseOffset + settingsWindowScroll.x, settingsWindowElementBaseOffset + settingsWindowScroll.y, 100, 50}, "test");
+    BeginScissorMode(settingsWindowGuiScrollPanelView.x, settingsWindowGuiScrollPanelView.y, settingsWindowGuiScrollPanelView.width, settingsWindowGuiScrollPanelView.height);
+      //GuiGrid((Rectangle){settingsWindowGuiWindowBoxRect.x + settingsWindowGuiScrollPanelScroll.x, settingsWindowGuiWindowBoxRect.y + settingsWindowGuiScrollPanelScroll.y,   settingsWindowGuiScrollPanelRect.width, settingsWindowGuiScrollPanelRect.height}, NULL, 16, 3, NULL);
+      //int res = GuiButton((Rectangle){settingsWindowElementBaseOffset + settingsWindowGuiScrollPanelScroll.x, settingsWindowElementBaseOffset + settingsWindowGuiScrollPanelScroll.y, 100, 50}, "test");
       DrawSettingsLabel("Clock Radius", 1, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 2, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 3, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 4, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 5, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 6, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 7, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 8, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 9, &cf.radius);
+      DrawSettingsLabel("Clock Radius", 10, &cf.radius);
       //DrawSettingsLabel("size", 2, &)settingsWindowElementBaseOffset);
     EndScissorMode();
     if (result > 0) *isSettingsWindowOpened = false;
@@ -30,8 +39,8 @@ void DrawSettingsWindow(bool *isSettingsWindowOpened)
 
 void DrawSettingsLabel(const char* settingName, int position, int* settingValue)
 {
-  GuiLabel((Rectangle){settingsWindowContentRect.x + 30 + settingsWindowScroll.x, settingsWindowContentRect.y + (30 * position) + settingsWindowScroll.y, 100, 24}, settingName);
-  //GuiButton((Rectangle){settingsWindowContentRect.x + 140 + settingsWindowScroll.x, settingsWindowContentRect.y + (30 * position) + settingsWindowScroll.y, 50, 24}, "tset button");
-  GuiValueBox((Rectangle){settingsWindowContentRect.x + 140 + settingsWindowScroll.x, settingsWindowContentRect.y + (30 * position) + settingsWindowScroll.y, 50, 24}, "test", settingValue, 0, 1000, 1);
+  GuiLabel((Rectangle){settingsWindowGuiWindowBoxRect.x + 30 + settingsWindowGuiScrollPanelScroll.x, settingsWindowGuiWindowBoxRect.y + (30 * position) + settingsWindowGuiScrollPanelScroll.y, 100, 24}, settingName);
+  //GuiButton((Rectangle){settingsWindowGuiScrollPanelRect.x + 140 + settingsWindowGuiScrollPanelScroll.x, settingsWindowGuiScrollPanelRect.y + (30 * position) + settingsWindowGuiScrollPanelScroll.y, 50, 24}, "tset button");
+  GuiValueBox((Rectangle){settingsWindowGuiWindowBoxRect.x + 140 + settingsWindowGuiScrollPanelScroll.x, settingsWindowGuiWindowBoxRect.y + (30 * position) + settingsWindowGuiScrollPanelScroll.y, 50, 24}, "test", settingValue, 0, 1000, 1);
 }
 
